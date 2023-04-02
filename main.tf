5a7eabd16422561a488297d4e06b5f5e8730f07f
Yoojin93 <yoojinbae93@gmail.com>
2023-04-02 15:14:09 -0500

    updated

--- main.tf
+++ main.tf
@@ -125,14 +125,14 @@ resource "aws_route_table_association" "private_b_assoc" {
 
 resource "aws_security_group" "nginx_sg" {
   name        = "nginx_sg"
-  description = "nginx security group"
+  description = "allow inbound traffic from ALB"
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
     description = "ALB SG connection"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     security_groups = [aws_security_group.alb_sg.id]
   }
 
@@ -146,13 +146,13 @@ resource "aws_security_group" "nginx_sg" {
 
 resource "aws_security_group" "alb_sg" {
   name        = "alb_sg"
-  description = "alb security group"
+  description = "allow inbound HTTP traffic"
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -160,6 +160,26 @@ resource "aws_security_group" "alb_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "wordpress_rds_sg"
+  description = "wordpress rds sg"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.nginx_sg.id]
+  }
+
+  egress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
