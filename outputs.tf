6692f986b2c41c6225103869086525173b1e4c25
Yoojin93 <yoojinbae93@gmail.com>
2023-04-02 15:23:39 -0500

    updated

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "load_balancer_dns" {
-    description = "Public URL for ALB"
-    value = aws_lb.nginx-alb.dns_name
+  description = "Public URL for ALB"
+  value       = aws_lb.nginx-alb.dns_name
 }
 
 output "db_name" {
-    description = "Database Name"
-    value = aws_db_instance.wordpress_rds_instance.db_name
+  description = "Database Name"
+  value       = aws_db_instance.wordpress_rds_instance.db_name
 }
