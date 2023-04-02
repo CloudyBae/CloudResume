b4a7e222feb8245f1b8a371cf26b0214119eb553
Yoojin93 <yoojinbae93@gmail.com>
2023-04-01 19:21:05 -0500

    datasources.tf

---
+++ datasources.tf
@@ -0,0 +1,12 @@
+# Linux (Ubuntu) AMI used for EC2 instances
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
