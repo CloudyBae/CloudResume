6da30bf215fad6d3021e7af343a624bb496f3ab1
Yoojin93 <yoojinbae93@gmail.com>
2023-04-01 21:54:15 -0500

    alb.tf

---
+++ alb.tf
@@ -0,0 +1,31 @@
+resource "aws_lb" "nginx-alb" {
+  name               = "nginx-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_subnet_a.id, aws_subnet.public_subnet_b.id]
+
+  
+
+  tags = {
+    Name = "Nginx-alb"
+  }
+}
+
+resource "aws_lb_target_group" "nginx-alb-target" {
+  name     = "nginx-alb-target"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+}
+
+resource "aws_lb_listener" "nginx-alb-listener" {
+  load_balancer_arn = aws_lb.nginx-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx-alb-target.arn
+  }
+}
