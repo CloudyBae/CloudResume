63addf442fdee6368d2aee760c2685484d1ac1a5
Yoojin93 <yoojinbae93@gmail.com>
2023-04-01 19:20:47 -0500

    asg.tf

---
+++ asg.tf
@@ -0,0 +1,29 @@
+resource "aws_autoscaling_group" "nginx_asg" {
+  name     = "Nginx-asg"
+  max_size = 3
+  min_size = 1
+  #   health_check_grace_period = 300
+  #   health_check_type         = "ELB"
+  desired_capacity     = 2
+  force_delete         = true
+  launch_configuration = aws_launch_configuration.webserver_launch_config.name
+  vpc_zone_identifier  = [aws_subnet.private_subnet_a.id]
+
+  tag {
+    key                 = "Name"
+    value               = "Nginx-web-server-asg"
+    propagate_at_launch = true
+  }
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [load_balancers, target_group_arns]
+  }
+}
+
+resource "aws_launch_configuration" "webserver_launch_config" {
+  name          = "web_config"
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  user_data     = file("userdata.tpl")
+}
+
